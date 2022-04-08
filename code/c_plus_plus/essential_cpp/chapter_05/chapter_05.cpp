ab33cb39a8acce54558774c8f5bb0e7ae156c421
LiladVV <13115046123@163.com>
2022-04-08 17:29:48 +0800

    添加第五章练习代码

--- code/c_plus_plus/essential_cpp/chapter_05/chapter_05.cpp
+++ code/c_plus_plus/essential_cpp/chapter_05/chapter_05.cpp
@@ -3,15 +3,19 @@
 #include "chapter_05.h"
 #include "libmat.h"
 #include "book.h"
+#include "audio_book.h"
 
 using namespace std;
 
 void Chapter_05()
 {
-    cout << "Creating a LibMat object to print.\n";
+//    cout << "Creating a LibMat object to print.\n";
 //    LibMat libmat;
 //    Print(libmat);
 
-    Book book("aaa", "bb");
-    Print(book);
+//    Book book("aaa", "bb");
+//    Print(book);
+
+    AudioBook audio("aaa", "111", "***");
+    audio.Print();
 }
