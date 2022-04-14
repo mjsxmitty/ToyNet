79c59813dc8963e10ecd460380ef16a25fb9a4eb
Luis Gao <52738474+LiladVV@users.noreply.github.com>
2022-04-14 08:57:06 +0800

    Delete code/Linux/uulp/build directory
    删除无用工程文件

--- code/Linux/uulp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gao_zhuo/share/uulp
-# Build directory: /home/gao_zhuo/share/uulp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
