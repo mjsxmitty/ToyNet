bd7e6b8835519ffe5e3606171f95303a82f14050
gao-zhuo <19524281496@163.com>
2022-10-15 23:37:32 +0800

    结构调整

--- code/c_plus_plus/cpp_primer/chapter_03/chapter_03.h
+++ code/c_plus_plus/cpp_primer/chapter_03/chapter_03.h
@@ -10,20 +10,23 @@ extern void ch_3_2_3();
 extern void hw_3_8();
 extern void hw_3_10();
 
+extern void ch_3_3();
 extern void ch_3_3_1();
 extern void ch_3_3_3();
 
+extern void ch_3_4();
 extern void ch_3_4_1();
-extern void hw_20();
+extern void hw_3_20();
 extern void hw_3_22();
 extern void hw_2_23();
 
 extern void ch_3_5();
 extern void ch_3_5_1();
 extern void ch_3_5_3();
-extern void hw_36();
+extern void hw_3_36();
 extern void ch_3_5_4();
 extern void ch_3_5_5();
+extern void hw_3_42();
 
 extern void ch_3_6();
 extern void hw_43_44_45();
