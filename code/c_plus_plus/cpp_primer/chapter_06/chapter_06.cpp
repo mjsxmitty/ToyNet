b87e1fedacfc9e5ce9e73a821b761e0146b7ef45
gao_zhuo <19524281496@163.com>
2022-10-21 10:35:55 +0800

    完成第六章

--- code/c_plus_plus/cpp_primer/chapter_06/chapter_06.cpp
+++ code/c_plus_plus/cpp_primer/chapter_06/chapter_06.cpp
@@ -13,16 +13,19 @@ using namespace std;
 void ch_06()
 {
     /* 函数基础 */
-    ch_6_1();
+    //ch_6_1();
     
     /* 参数传递 */
-    ch_6_2();
+    //ch_6_2();
 
     /* 返回类型和return语句 */
-    ch_6_3();
+    //ch_6_3();
 
-    /*  */
-    ch_6_5();
+    /* 特殊特性语言用途 */
+    //ch_6_5();
+
+    /* 函数指针 */
+    ch_6_7();
 }
 
 /* 编写函数 */
@@ -281,7 +284,7 @@ void ch_6_3_2()
 /* 数组类型相关 */
 int Arr[10];            // int  数组
 int *pArr[10];          // int* 数组
-int (*p)[10] = &Arr;    // 指向数组的指针
+int (*p)[10] = &Arr;    // 指向数组的指针 
 
 /* 数组类型别名 */
 //using arrT = int [5];
@@ -295,6 +298,8 @@ int     (*Func(int i))[5];          //直接声明
 int odd[] = {1,3,5,7,9};
 int even[] = {0,2,4,6,8};
 
+typedef decltype(odd) *arry_ptr;
+
 int* ElemPtr(int i)
 {
     return (i % 2) ? odd : even;
@@ -303,7 +308,8 @@ int* ElemPtr(int i)
 //arrT* Func(int i)
 //auto Func(int i) -> int (*)[5]
 //int (*Func(int i))[5]
-decltype(odd)* ArrPtr(int i)
+//decltype(odd)* ArrPtr(int i)
+arry_ptr ArrPtr(int i)
 {
     return (i % 2) ? &odd : &even;
 }
@@ -344,25 +350,25 @@ void ch_6_3_3()
 
 void ch_6_5()
 {
+    /* 默认实参 */
     ch_6_5_1();
+
+    /* 内联函数和constexptr函数 */
+    ch_6_5_2();
 }
 
 typedef string::size_type sz;
-//string screen(sz ht = 24, sz wid = 80, char bkgrnd = ' ');
-//string screen(sz ht, sz wid, char bkgrnd = ' ');
+//string Screen(sz ht = 24, sz wid = 80, char bkgrnd = ' ');
+string Screen(sz ht, sz wid, char bkgrnd = ' ');
+//string Screen(sz ht, sz wid, char bkgrnd = '*');  // error
 
-//string screen(sz ht, sz wid, char bkgrnd = '*');  // error
-
-//string screen(sz ht, sz wid = 80, char bkgrnd);
-//string screen(sz ht = 10, sz wid, char bkgrnd);
-//string screen(sz ht = 10, sz wid = 80, char bkgrnd);
+string Screen(sz ht, sz wid = 80, char bkgrnd);
+//string Screen(sz ht = 10, sz wid, char bkgrnd);
+//string Screen(sz ht = 10, sz wid = 80, char bkgrnd);
 
 sz wd = 111;
 char def = '*';
-sz ht()
-{
-    return 123;
-}
+sz ht() { return 123; }
 
 string screen(sz p1 = ht(), sz p2 = wd, char p3 = def)
 {
@@ -376,16 +382,14 @@ string screen(sz p1 = ht(), sz p2 = wd, char p3 = def)
 void ch_6_5_1()
 {
 
+    /* 默认食材初始值 */
     screen(10);
 
     def = '#';
     //sz wd = 1;
-
     screen();
 }
 
-constexpr int NewSize() {return 10;}
-constexpr size_t Scale(size_t cnt) {return  NewSize() * cnt;}
 void ch_6_5_2()
 {
     int Arr[Scale(2)] = {0};
@@ -393,21 +397,99 @@ void ch_6_5_2()
     //int Arr2[Scale(i)] = {0};
 }
 
+#define NDEBUG
+
 void ch_6_5_3()
 {
     //assert(false);
     assert(true);
 }
 
-string::size_type SumLength(const string &s1, const string &s2)
+void ch_6_7()
 {
-    return s1.size() + s2.size();
+    /* 使用函数指针 */
+    {
+        /*
+        bool LengthCompare(const string &, const string &);
+        bool (*pf)(const string &, const string &);
+
+        pf = LengthCompare;
+        pf = &LengthCompare;
+        pf = 0;
+        //bool ret1 = pf("hello", "nihao");
+        //bool ret2 = (*pf)("hello", "nihao");
+        //bool ret3 = LengthCompare("hello", "nihao");
+
+        string::size_type SumLength(const string &, const string &);
+        //pf = SumLength;
+        */
+    }
+
+    /* 重载函数指针 */
+    {
+        /*
+        void ff(int *);
+        void ff(unsigned int);
+
+        void (*pf1)(unsigned int) = ff;
+//        void (*pf2)(int) = ff;
+//        double (*pf3)(unsigned int) = ff;
+        */
+    }
+
+    /* 函数指针形参 */
+    {
+        /*
+        bool LengthCompare(const string &, const string &);
+
+        void UseBigger(const string &, const string &,
+                       bool pf(const string &, const string &));
+        void UseBigger(const string &, const string &,
+                       bool (*pf)(const string &, const string &));
+
+        string s1("hello"), s2("nihao");
+        UseBigger(s1, s2, LengthCompare);
+
+        typedef bool Func(const string &, const string &);
+        typedef decltype (LengthCompare) Func;
+        typedef bool (*FuncP)(const string &, const string &);
+        typedef decltype (LengthCompare) *FuncP;
+        using Fp = bool (*)(const string &, const string &);
+
+        void UseBigger(const string &, const string &, Func);
+        void UseBigger(const string &, const string &, FuncP);
+        void UseBigger(const string &, const string &, Fp);
+        */
+    }
+
+    /* 返回指向函数的指针 */
+    {
+        /*
+        using F = int (int *, int);
+        using PF = int (*)(int *, int);
+
+        PF f1(int);
+//        F f2(int);
+        F* f3(int);
+
+        int (*pff(int))(int *, int);
+
+//        typedef int (f)(int *, int);
+//        int temp(int *, int);
+//        typedef decltype(temp) *pt;
+//        pt ft(int);
+//        typedef decltype(temp) fp;
+//        fp* ftt(int);
+        */
+    }
+
+    ch_6_7_test_1();
+
+    ch_6_7_test_2();
+
+    hw_54_55_56();
 }
 
-string::size_type LargerLength(const string &s1, const string &s2)
-{
-    return (s1.size() > s2.size()) ? s1.size() : s2.size();
-}
 
 int MinElement(vector<int>::iterator beg, vector<int>::iterator end)
 {
@@ -423,7 +505,7 @@ int MinElement(vector<int>::iterator beg, vector<int>::iterator end)
 
 int (*pf)(vector<int>::iterator beg, vector<int>::iterator end) = MinElement;
 
-void ch_6_7_1()
+void ch_6_7_test_1()
 {
     vector<int> ivec;
     // give ivec some values
@@ -435,6 +517,16 @@ void ch_6_7_1()
 
 	cout << "equivalent indirect call: "
 	     << (*pf)(ivec.begin(), ivec.end()) << endl;
+}
+
+string::size_type SumLength(const string &s1, const string &s2)
+{
+    return s1.size() + s2.size();
+}
+
+string::size_type LargerLength(const string &s1, const string &s2)
+{
+    return (s1.size() > s2.size()) ? s1.size() : s2.size();
 }
 
 /*返回指向函数的指针*/
@@ -449,44 +541,33 @@ decltype(SumLength)* GetFun(const string &fetch)
     return LargerLength;
 }
 
-void ch_6_7_2()
+void ch_6_7_test_2()
 {
     cout <<GetFun("sum")("hello", "world") << endl;
     cout <<GetFun("larger")("hello", "world") << endl;
 }
 
-int Func1(int a, int b)
-{
-    return a + b;
-}
+int Func1(int a, int b){return a + b;}
+int Func2(int a, int b){return a - b;}
+int Func3(int a, int b){return a * b;}
+int Func4(int a, int b){return a / b;}
+int Func5(int a, int b){return a % b;}
 
-int Func2(int a, int b)
-{
-    return a - b;
-}
+typedef int pF(int a, int b);
+typedef int (*pFp)(int a, int b);
+//using pF = int (int, int);
+//using pFp = int (*)(int a, int b);
+//typedef decltype(Func1) pF;
+//typedef decltype(Func1) *pFp;
 
-int Func3(int a, int b)
+//void Complute(int a, int b, int (*p)(int, int))
+void Complute(int a, int b, pFp para)
 {
-    return a * b;
-}
-
-int Func4(int a, int b)
-{
-    return a / b;
-}
-
-int Func5(int a, int b)
-{
-    return a % b;
-}
-
-void Complute(int a, int b, int (*p)(int, int))
-{
-    std::cout << p(a, b) << std::endl;
+    cout << para(a, b) << endl;
 }
 
 //回调
-void ch()
+void hw_54_55_56()
 {
     int i = 5, j = 10;
     decltype(Func1) *p1 = Func1, *p2 = Func2, *p3 = Func3, *p4 = Func4, *p5 = Func5;
@@ -496,14 +577,3 @@ void ch()
 
     return ;
 }
-
-//函数别名
-// using GetFun = string::size_type (*)(const string &, const string &);
-// typedef string::size_type (*GetFun)(const string&, const string &);
-
-//函数类型别名
-typedef string::size_type GetFun1(const string &, const string &);
-typedef decltype (LargerLength) GetFun1;
-//函数指针别名
-typedef string::size_type (*GetFun2)(const string&, const string);
-typedef decltype (LargerLength) *GetFun3;
--- code/c_plus_plus/cpp_primer/chapter_06/chapter_06.h
+++ code/c_plus_plus/cpp_primer/chapter_06/chapter_06.h
@@ -24,12 +24,21 @@ extern void ch_6_3_3();
 
 extern void ch_6_5();
 extern void ch_6_5_1();
+
+constexpr int NewSize() {return 10;}
+constexpr size_t Scale(size_t cnt)
+{
+    typedef std::string::size_type sz;
+    return  NewSize() * cnt;
+}
+
 extern void ch_6_5_2();
 extern void ch_6_5_3();
 
 extern void ch_6_7();
-
-extern void ch();
+extern void ch_6_7_test_1();
+extern void ch_6_7_test_2();
+extern void hw_54_55_56();
 
 struct ErrCode
 {
@@ -43,12 +52,6 @@ struct ErrCode
     }     
     int num;    
 };
-
-//默认实参声明
-typedef std::string::size_type sz;
-std::string Screen1(sz, sz, char = ' ');
-//std::string Screen1(sz, sz, char = '*');
-std::string Screen1(sz, sz = 100, char);
 
 
 #endif // __CPP_PRIMER_CHAPTER_06_H__
