5242bf7768dc427a6538c0afd92a488abb947d34
gaucho <13630575810@163.com>
2023-11-18 21:01:32 +0800

    添加文件

--- code/cpp/effective_cpp/item/item_02.cpp
+++ code/cpp/effective_cpp/item/item_02.cpp
@@ -1,4 +1,23 @@
 
 #include "item_02.h"
+#include <vector>
+#include <iostream>
 
+using namespace std;
 
+void Item02()
+{
+    cout << "**********item 01**********" << endl;
+#if 1
+    std::vector<int> ivec(10, 1024);
+    const std::vector<int>::iterator iter = ivec.begin();
+    *iter = 0;
+    //++iter;
+
+    std::vector<int>::const_iterator citer = ivec.cbegin();
+    //*citer = 10;
+    ++citer;
+#endif
+
+    cout << "***************************" << endl;
+}
--- code/cpp/effective_cpp/item/item_02.h
+++ code/cpp/effective_cpp/item/item_02.h
@@ -2,4 +2,9 @@
 #ifndef __EFFECTIVE_CPP_ITEM_02_H__
 #define __EFFECTIVE_CPP_ITEM_02_H__
 
+void Item02();
+
+class Rational{};
+
+
 #endif // __EFFECTIVE_CPP_ITEM_02_H__
