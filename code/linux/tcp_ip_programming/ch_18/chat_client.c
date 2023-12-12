b1747d11cca33a449af3f75ebe687a11786dd9a2
gaucho <13630575810@163.com>
2023-12-12 10:19:39 +0800

    完成聊天程序

--- code/linux/tcp_ip_programming/ch_18/chat_client.c
+++ code/linux/tcp_ip_programming/ch_18/chat_client.c
@@ -11,8 +11,86 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define BUFF_SIZE   1024
+#define NAME_SIZE   20
+
+char name[NAME_SIZE];
+char msg[BUFF_SIZE];
+
+void* send_msg(void *arg)
+{
+    int sock = *((int *)arg);
+    char send_msg[NAME_SIZE + BUFF_SIZE];
+
+    while (1) {
+        fgets(msg, BUFF_SIZE, stdin);
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+            close(sock);
+            break;
+        }
+            
+        sprintf(send_msg, "%s %s", name, msg);
+        write(sock, send_msg, strlen(send_msg));
+    }
+
+    return NULL;
+}
+
+void* recv_msg(void *arg)
+{
+    int sock = *((int *)arg);
+    char recv_msg[NAME_SIZE + BUFF_SIZE];
+    int str_len;
+
+    while (1) {
+        str_len = read(sock, recv_msg, NAME_SIZE + BUFF_SIZE + 1);
+        if (str_len == -1)
+            return (void *)-1;
+
+        recv_msg[str_len] = 0;
+        fputs(recv_msg, stdout);
+    }
+
+    return NULL;
+}
+
+
 int main(int argc, char **argv)
 {
+    int                 sock;
+    struct sockaddr_in  server_addr;
 
+    pthread_t   send_thread, recv_thread;
+    void        *thread_return;
+    
+    if (argc != 4) {
+        fprintf(stderr, "usage: %s <ip> <port> <name>.\n", argv[0]);
+        exit(-1);
+    }
+
+    sprintf(name, "[%s]:", argv[3]);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        exit(-1);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect");
+        exit(-1);
+    }
+    printf("connected ...\n");
+
+    pthread_create(&send_thread, NULL, send_msg, (void *)&sock);
+    pthread_create(&recv_thread, NULL, recv_msg, (void *)&sock);
+    pthread_join(send_thread, &thread_return);
+    pthread_join(recv_thread, &thread_return);
+
+    close(sock);
     return 0;
 }
--- code/linux/tcp_ip_programming/ch_18/chat_server.c
+++ code/linux/tcp_ip_programming/ch_18/chat_server.c
@@ -19,8 +19,39 @@ int client_cnt;
 int client_sockets[MAX_CLIENTS];
 pthread_mutex_t mutex;
 
-void handle_client(void *)
+void send_msg(char *msg, int len)
 {
+    int i;
+    pthread_mutex_lock(&mutex);
+    for (i = 0; i < client_cnt; ++i)        // 发送给所有客户端
+        write(client_sockets[i], msg, len);
+    pthread_mutex_unlock(&mutex);
+}
+
+void handle_client(void *arg)
+{
+    int client_socket = *((int *)arg);
+    char buf[BUFF_SIZE];
+    int str_len, i;
+
+    while ((str_len = read(client_socket, buf, sizeof(buf))) != 0)
+        send_msg(buf, str_len);
+
+    // 移除
+    pthread_mutex_lock(&mutex);
+    for (i = 0; i < client_cnt; ++i) {
+        if (client_socket == client_sockets[i]) {
+            while (i < client_cnt - 1) {
+                client_sockets[i] = client_sockets[i+1];
+                i++;
+            }
+            break;
+        }
+    }
+
+    client_cnt--;
+    pthread_mutex_unlock(&mutex);
+    close(client_socket);
 
 }
 
@@ -38,8 +69,16 @@ int main(int argc, char **argv)
         exit(-1);
     }
 
+    pthread_mutex_init(&mutex, NULL);
+
+    server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("socket");
+        exit(-1);
+    }
+
     memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = PF_INET;
+    server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(atoi(argv[1]));
 
@@ -55,14 +94,21 @@ int main(int argc, char **argv)
 
     while (1) {
         client_addr_size = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *)&client_socket, &client_addr_size);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
         if (client_socket == -1) {
             perror("accept");
             break;
         }
 
-        
+        pthread_mutex_lock(&mutex);
+        client_sockets[client_cnt++] = client_socket;
+        pthread_mutex_unlock(&mutex);
+
+        pthread_create(&pid, NULL, handle_client, (void *)&client_socket);
+        pthread_detach(pid);
+        printf("connect client: %s\n", inet_ntoa(client_addr.sin_addr));
     }
-    
+
+    close(server_socket);
     return 0;
 }
